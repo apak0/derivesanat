6a019008c9c0ce25649dd04d08d39281353efe3f
Muhammed Apak <muhampak25@gmail.com>
2024-03-11 23:59:15 +0300

    Footer component created

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="flex-shrink-0">
+          <p>&copy; 2024 Company Name. All rights reserved.</p>
+        </div>
+        <div className="flex items-center">
+          <a href="#" className="text-gray-400 hover:text-white mr-4">
+            Link 1
+          </a>
+          <a href="#" className="text-gray-400 hover:text-white mr-4">
+            Link 2
+          </a>
+          <a href="#" className="text-gray-400 hover:text-white">
+            Link 3
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
