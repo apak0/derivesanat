9fa6be04e1c31d186f04a92c00c23a656f300b7a
apak0 <muhampak25@gmail.com>
2024-03-18 10:58:57 +0300

    container component configured

--- src/components/container/index.js
+++ src/components/container/index.js
@@ -2,10 +2,21 @@ import React from "react";
 
 function Container() {
   return (
-    <div className="min-h-screen bg-red-500 flex flex-col">
-      <div className="flex-grow"> {/* Bu kısım Navbar ve Footer'ı engellemeyecek */}
-        Container
+    <div>
+      <div className="  h-16 bg-yellow-400 ">
+        Carausel
       </div>
+    
+    <div className="min-h-screen bg-red-200 flex">
+      <div className="flex-grow px-60 py-36 ">
+        <div className="border-4 border-indigo-600 h-full flex flex-wrap">
+          <div className="w-10 h-10 border-2 border-yellow-600">Box 1</div>
+          <div className="w-10 h-10 border-2 border-yellow-600">Box 2</div>
+          <div className="w-10 h-10 border-2 border-yellow-600">Box 3</div>
+          <div className="w-10 h-10 border-2 border-yellow-600">Box 4  </div>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
