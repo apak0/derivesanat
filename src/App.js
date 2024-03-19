8db8e35b014e3c3d3ea06cfbb9b75606e933e4d2
apak0 <muhampak25@gmail.com>
2024-03-19 16:42:43 +0300

    footer position al sticky at bottom

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import Navbar from "./components/navbar";
 import Container from "./components/container";
 import Footer from "./components/footer";
 import TopNav from "./components/topnav/TopNav";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AboutUs from "./components/pages/aboutUs/AboutUs";
+import ContactUs from "./components/pages/contactUs/ContactUs";
+import Products from "./components/pages/products/Products";
 
 function App() {
   return (
-    <div className="app-container">
-      <TopNav/>
-      <Navbar className="navbar" />
-      <Container className="container" />
-      <Footer className="footer" />
-    </div>
+    <Router>
+      <TopNav />
+      <Navbar />
+      <div className="app-container">
+        <Routes>
+          <Route path="/" exact element={<Container />} />
+          <Route path="/products" exact element={<Products />} />
+          <Route path="/contactus" exact element={<ContactUs />} />
+          <Route path="/aboutus" exact element={<AboutUs />} />
+        </Routes>
+      </div>
+      <Footer/>
+    </Router>
   );
 }
 
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -24,15 +24,16 @@ function Navbar() {
     <nav className={`nav ${scrolled ? "scrolled" : ""}`}>
       <div className="w-full mx-auto px-4 ">
         <div className="flex justify-around items-center ">
-
           {/* Logo  */}
           <div className="flex-shrink-0">
             <div className="text-white font-bold text-2xl">
-              <img
-                className="w-full h-20 object-cover object-center rounded-full"
-                src={require("./leather_logo.jpg")}
-                alt="Slide 1"
-              />
+              <a href="/">
+                <img
+                  className="w-full h-20 object-cover object-center rounded-full"
+                  src={require("./leather_logo.jpg")}
+                  alt="Slide 1"
+                />
+              </a>
             </div>
           </div>
 
@@ -40,19 +41,25 @@ function Navbar() {
           <div className="hidden md:flex md:items-center">
             <div className="ml-auto space-x-4">
               <a
-                href="#"
+                href="/"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-xl font-medium"
+              >
+                Ana Sayfa
+              </a>
+              <a
+                href="/products"
                 className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-xl font-medium"
               >
                 Ürünler
               </a>
               <a
-                href="#"
+                href="/aboutus"
                 className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-xl font-medium"
               >
                 Hakkımızda
               </a>
               <a
-                href="#"
+                href="contactus"
                 className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-xl font-medium"
               >
                 Bize Ulaşın
