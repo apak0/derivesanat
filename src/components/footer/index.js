ec3a1072f21093c4f5a31cb4e95649529605d240
Muhammed Apak <muhampak25@gmail.com>
2024-07-02 23:53:25 +0300

    footer social links bug fixed

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,24 +5,43 @@ function Footer() {
     <footer className="bg-gray-800 text-white py-4  bottom-0 w-full">
       <div className="container mx-auto flex justify-between items-center">
         <div>
-          <p className="text-sm">&copy; 2024 Deri ve Sanat. All rights reserved.</p>
-          <p className="text-sm">Cumhuriyet Mahallesi Akasya Sokak No:2 Gemlik/BURSA</p>
+          <p className="text-sm">
+            &copy; 2024 Deri ve Sanat. All rights reserved.
+          </p>
+          <p className="text-sm">
+            Cumhuriyet Mahallesi Akasya Sokak No:2 Gemlik/BURSA
+          </p>
           <p className="text-sm">Tel: 0244 519 87 65</p>
         </div>
         <div>
           <ul className="flex space-x-4">
             <li>
-              <a href="www.facebook.com" target="_blank"  className="text-gray-400 hover:text-white">
+              <a
+                href="https://www.facebook.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-400 hover:text-white transition-colors duration-300"
+              >
                 Facebook
               </a>
             </li>
             <li>
-              <a href="www.twitter.com" target="_blank" className="text-gray-400 hover:text-white">
+              <a
+                href="https://www.twitter.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-400 hover:text-white transition-colors duration-300"
+              >
                 Twitter
               </a>
             </li>
             <li>
-              <a href="www.instagram.com" target="_blank" className="text-gray-400 hover:text-white">
+              <a
+                href="https://www.instagram.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-400 hover:text-white transition-colors duration-300"
+              >
                 Instagram
               </a>
             </li>
